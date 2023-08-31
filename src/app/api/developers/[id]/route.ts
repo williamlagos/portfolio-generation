70ce44136f1ddd2393b06c2a3c5d891474af59f6
William Oliveira de Lagos <william.lagos@icloud.com>
2023-08-31 21:36:40 +0200

    feat: added error handling over the crud operations

--- src/app/api/developers/[id]/route.ts
+++ src/app/api/developers/[id]/route.ts
@@ -4,24 +4,36 @@ import { NextResponse, NextRequest } from "next/server";
 const prisma = new PrismaClient()
 
 export const GET = async (_: NextRequest, context: { params: { id: string } }) => {
-  const developers = await prisma.developer.findUniqueOrThrow({ 
-    where: { id: parseInt(context.params.id) }
-  })
-  return NextResponse.json({ developers })
+  try {
+    const developers = await prisma.developer.findUniqueOrThrow({ 
+      where: { id: parseInt(context.params.id) }
+    })
+    return NextResponse.json({ developers }) 
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 404 })
+  }
 }
 
 export const PUT = async (request: NextRequest, context: { params: { id: string } }) => {
-  const data = await request.json()
-  await prisma.developer.update({
-    where: { id: parseInt(context.params.id) },
-    data
-  })
-  return new Response(null, { status: 204 })
+  try {
+    const data = await request.json()
+    await prisma.developer.update({
+      where: { id: parseInt(context.params.id) },
+      data
+    })
+    return new Response(null, { status: 204 })
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 404 })
+  }
 }
 
 export const DELETE = async (_: NextRequest, context: { params: { id: string } }) => {
-  await prisma.developer.delete({
-    where: { id: parseInt(context.params.id) }
-  })
-  return new Response(null, { status: 204 })
+  try {
+    await prisma.developer.delete({
+      where: { id: parseInt(context.params.id) }
+    })
+    return new Response(null, { status: 204 }) 
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 404 })
+  }
 }
--- src/app/api/developers/route.ts
+++ src/app/api/developers/route.ts
@@ -4,9 +4,13 @@ import { NextResponse, NextRequest } from "next/server";
 const prisma = new PrismaClient()
 
 export const POST = async (request: NextRequest) => {
-  const data = await request.json()
-  const user = await prisma.developer.create({ data })
-  return NextResponse.json(user, { status: 201 })
+  try {
+    const data = await request.json()
+    const user = await prisma.developer.create({ data })
+    return NextResponse.json(user, { status: 201 }) 
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 })
+  }
 }
 
 export const GET = async () => {
